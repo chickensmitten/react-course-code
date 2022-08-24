9ef8831b1b0aacb34f21898d4c9a29844a399c79
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 09:16:00 +0800

    output an array of lists

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,11 +1,38 @@
-
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
 
 function AllMeetupsPage() {
   return (
-    <div>
-      AllMeetupsPage
-    </div>
-  )
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {[<li key="1">List 1</li>,<li key="2">List 2</li>, <li key="3">List 3</li>]}
+      </ul>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default AllMeetupsPage
