f830f198d1a739321412bd72fab0b22e15ba7022
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 11:14:10 +0800

    use react createcontext instead of redux to reduce many states and props

---
+++ src/store/favorites-context.js
@@ -0,0 +1,20 @@
+const { createContext, useState } = require("react");
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0
+});
+
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length
+  };
+
+  return(
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  )
+}
