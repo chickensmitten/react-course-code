58c378caa80216db20da65fab76fe4cdc970506e
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 11:32:42 +0800

    demonstrating global state management with context
    this is the URL to the course https://www.udemy.com/course/nextjs-react-the-complete-guide/learn/lecture/25146706#questions

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,29 @@
+
+
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-context';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address
+      });
+    }
+  }
+
   return (
     <Card>
       <li className={classes.item}>
@@ -14,7 +36,11 @@ function MeetupItem(props) {
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button
+            onClick={toggleFavoriteStatusHandler}
+            >
+            { itemIsFavorite ? "Remove from Favorites": "Add to Favorites"}    
+          </button>
         </div>
       </li>
     </Card>
--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -2,10 +2,13 @@ const { createContext, useState } = require("react");
 
 const FavoritesContext = createContext({
   favorites: [],
-  totalFavorites: 0
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {}
 });
 
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
   function addFavoriteHandler(favoriteMeetup) {
@@ -13,7 +16,7 @@ function FavoritesContextProvider(props) {
       return prevUserFavorites.concat(favoriteMeetup);
     });
   }
-  
+
   function removeFavoriteHandler(meetupId) {
     setUserFavorites((prevUserFavorites) => {
       return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
@@ -26,7 +29,10 @@ function FavoritesContextProvider(props) {
 
   const context = {
     favorites: userFavorites,
-    totalFavorites: userFavorites.length
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler
   };
 
   return(
@@ -35,3 +41,5 @@ function FavoritesContextProvider(props) {
     </FavoritesContext.Provider>
   )
 }
+
+export default FavoritesContext;
