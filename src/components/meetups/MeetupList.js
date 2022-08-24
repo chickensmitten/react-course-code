d23ad0f7e584fdb7eb4285170f7158c2f2ee328b
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 11:04:06 +0800

    lessons on useEffect & transform json to array

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -4,16 +4,16 @@ import classes from "./MeetupList.module.css";
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
-      {props.meetups.map(meetup => 
+      {props.meetups.map((meetup) => (
         <MeetupItem 
-          key={meetup.id} 
-          id={meetup.id} 
-          image={meetup.image} 
-          title={meetup.title}
-          address={meetup.address}
-          description={meetup.description}
-        />
-      )}
+        key={meetup.id} 
+        id={meetup.id} 
+        image={meetup.image} 
+        title={meetup.title}
+        address={meetup.address}
+        description={meetup.description}
+      />
+      ))}
     </ul>
   );
 }
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
@@ -7,14 +7,31 @@ function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://udemy-max-react-course-code-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json"
-  ).then(response => {
-    return response.json();
-  }).then(data => {
-    setIsLoading(false);
-    setLoadedMeetups(data);
-  });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://udemy-max-react-course-code-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json"
+    ).then(response => {
+      return response.json();
+    }).then(data => {
+      // needs to tranform data cause firebase returns data in json when we need it in array
+      const meetups = [];
+      for (const key in data) {
+        const meetup ={
+          id: key,
+          ...data[key]
+        };
+        meetups.push(meetup);
+      }
+
+      setIsLoading(false);
+      setLoadedMeetups(meetups); 
+    });
+  }, []);
+  // when the dependency for useEffect is an empty array, there are no dependecy, 
+  // then the fetch function only executes one time and first time. subsequent changes in the react component, it won't execute again.
+  // if you put something like isLoading, whenever isLoading is changes, then useEffect will execute again.
+  // useEffect helps with avoid infinite loop from useState
 
   if (isLoading) {
     return (
