5956dc04f44a81add0b9c55e6af586a7394226c6
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 08:50:02 +0800

    restart to learn routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>    
-      <Todo text="Learn React" />
-    </div>    
-  );
+  return <div></div>;
 }
 
 export default App;
